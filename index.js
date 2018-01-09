7bdf6cd9dc7767c4a6154ba5238b2d935422faeb
ianagez <ianagez88@gmail.com>
2018-01-09 18:02:37 +0200

    sysAdminMsgConfig in process

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 var express = require('express');
+var fs = require('fs');
+var path = require('path');
 var app = express();
+const disk = require('diskusage');
 var database = require('./database');
 var bot=require('./telBot.js');
 
@@ -11,5 +14,54 @@ database.connectToDB(function(err){
 
     }
 });
+
+//getDiscUsageInfo();
+getLastBackupFile();
+
+function getDiscUsageInfo(){ console.log("getDiscUsageInfo");
+    try {
+        var configObj = JSON.parse(fs.readFileSync(path.join(__dirname, './config.json'))); console.log("configObj=",configObj);
+    }catch(e){
+        console.log("e 22=",e);
+        return;
+    }
+    if(!configObj || ! configObj.sysadminsMsgConfig) return;
+    var sysadminsMsgConfig=configObj.sysadminsMsgConfig;
+    var system = sysadminsMsgConfig.system ? sysadminsMsgConfig.system : null;
+    var backup = sysadminsMsgConfig.backup ? sysadminsMsgConfig.backup : null;
+
+    disk.check(system, function(err, info) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log("available /", info.available/1073741824);
+            console.log("free /",info.free/1073741824);
+            console.log("total /",info.total/1073741824);
+            disk.check(backup, function(err, info) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    console.log("available /home", info.available/1073741824);
+                    console.log("free /home", info.free/1073741824);
+                    console.log("total /home",info.total/1073741824);
+                }
+            });
+        }
+    });
+
+}
+
+function getLastBackupFile(){  console.log("getLastBackupFile=");
+    try{
+        var files = fs.readdirSync(path.join(__dirname,'./backup'));
+    }catch(e){
+        console.log("data=",data);
+    }
+    console.log("files[0]=",files[0]);
+    fs.stat(path.join(__dirname,'./backup/'+files[0]), function(err, stat){
+        console.log("err=",err);
+        console.log("stat=",stat);
+    })
+}
 
 app.listen(8182);
