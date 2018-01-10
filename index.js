ffb2dd7d5a34b36208b6af1d2c0174f042a28822
ianagez <ianagez88@gmail.com>
2018-01-10 13:46:40 +0200

    schedule sysadmin msg main functionality

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ var express = require('express');
 var fs = require('fs');
 var path = require('path');
 var app = express();
-const disk = require('diskusage');
+var disk = require('diskusage');
 var database = require('./database');
+var cron = require('node-cron');
 var bot=require('./telBot.js');
+var moment = require('moment');
 
 
 
@@ -15,53 +17,113 @@ database.connectToDB(function(err){
     }
 });
 
-//getDiscUsageInfo();
-getLastBackupFile();
+var scheduleAdminMsg;
+function startSendAdminMsgBySchedule(){
+    var serverConfig=getServerConfig();
+    var schedule=serverConfig.sysadminsSchedule;
+    if(!schedule) schedule='*/15 * * * * *';
+    var valid = cron.validate(schedule);
+    if(valid==false){
+        console.log("invalide cron format");
+        return;
+    }
+    var sysadminsMsgConfig = serverConfig.sysadminsMsgConfig;
+    if(!sysadminsMsgConfig) return;
+    if(scheduleAdminMsg)scheduleAdminMsg.destroy();
+    scheduleAdminMsg =cron.schedule(schedule, function(){
+        var diskSpase= getDiscUsageInfo(sysadminsMsgConfig);
+        var lastBackpupFile = getLastBackupFile(sysadminsMsgConfig);
+        var adminMsg='';
+        if(diskSpase && diskSpase.system){
+            adminMsg += "Ресурс: \n System: объем:"+diskSpase.system.total+"Гб, свободно:"+diskSpase.system.free+ "Гб ("+diskSpase.system.freePercent +"%).";
+        }
+        if(diskSpase && diskSpase.backup){
+            adminMsg += "\n Ресурс: \n Backup: объем:"+diskSpase.backup.total+"Гб, свободно:"+diskSpase.backup.free+ "Гб ("+diskSpase.backup.freePercent +"%).";
+        }
+        if(lastBackpupFile && lastBackpupFile.backupDate && lastBackpupFile.fileName){
+            adminMsg+="\n Последняя резервная копия БД "+ lastBackpupFile.fileName+" от " + moment(lastBackpupFile.backupDate).format("DD-MM-YYYY HH:mm:ss");
+        }
+        console.log("adminMsg=",adminMsg);
+        if(adminMsg)   bot.sendMsgToAdmins(adminMsg);
+    });
+    scheduleAdminMsg.start();
+}
+startSendAdminMsgBySchedule();
 
-function getDiscUsageInfo(){ console.log("getDiscUsageInfo");
+function getDiscUsageInfo(sysadminsMsgConfig) {
+    console.log("getDiscUsageInfo");
+    var system = sysadminsMsgConfig.system ? sysadminsMsgConfig.system.trim() : "";
+    var backup = sysadminsMsgConfig.backup ? sysadminsMsgConfig.backup.trim() : "";
+    var diskSpase = {};
+    if (system) {
+        try {
+            let info = disk.checkSync(system);
+            diskSpase.system = {};
+            diskSpase.system.total = parseInt (info.total / 1073741824);
+            diskSpase.system.free = parseInt(info.free / 1073741824);
+            diskSpase.system.freePercent = parseInt(diskSpase.system.free*100/diskSpase.system.total);
+            console.log("diskSpase.system=",diskSpase.system)
+        }
+        catch (err) {
+            console.log(err);
+            return;
+        }
+    }
+    if (backup) {
+        try {
+            let info = disk.checkSync(backup);
+            diskSpase.backup = {};
+            diskSpase.backup.total = parseInt(info.total / 1073741824);
+            diskSpase.backup.free = parseInt(info.free / 1073741824);
+            diskSpase.backup.freePercent =parseInt( diskSpase.backup.freePercent = diskSpase.backup.free*100/diskSpase.backup.total);
+                console.log("diskSpase.backup=",diskSpase.backup);
+        }
+        catch (err) {
+            console.log(err);
+            return;
+        }
+    }
+    return diskSpase;
+}
+
+function getLastBackupFile(sysadminsMsgConfig){
+    var backup = sysadminsMsgConfig.backup ? sysadminsMsgConfig.backup.trim() : "";
+    if(!backup) return;
+    var backupFileName=sysadminsMsgConfig.dbBackupFileName ? sysadminsMsgConfig.dbBackupFileName.trim():"";
+    if(!backupFileName) return;
+    var backupFileNameTemplate=new RegExp("^"+backupFileName.substring(0,backupFileName.indexOf("*"))/* +" /\\w{0, }\\"+backupFileName.substring(backupFileName.indexOf("*")+1)+"$"*/);
+    try{
+       // var files = fs.readdirSync(path.join(__dirname,'./backup'));
+        var files = fs.readdirSync(backup);
+    }catch(e){
+        console.log("error=",e);
+    }
+    var lastBackpupFile={};
+    lastBackpupFile.backupDate=0;
+    lastBackpupFile.fileName="";
+    for (var i in files){
+        var stat = fs.lstatSync(__dirname,'./backup/'+files[i]);
+        if(files[i].match(backupFileNameTemplate)){                     console.log("files[i] match=",files[i]);
+            if(stat.birthtime>lastBackpupFile.backupDate){
+                lastBackpupFile.backupDate=stat.birthtime;
+                lastBackpupFile.fileName=files[i];
+             }
+        }
+    }
+    console.log("lastBackupDate=",lastBackpupFile.backupDate);
+    console.log("lastBackupFileName=",lastBackpupFile.fileName);
+    return  lastBackpupFile;
+}
+//   '/^Б/' +/т$/
+
+function getServerConfig(){
     try {
         var configObj = JSON.parse(fs.readFileSync(path.join(__dirname, './config.json'))); console.log("configObj=",configObj);
     }catch(e){
-        console.log("e 22=",e);
+        console.log("error=",e);
         return;
     }
-    if(!configObj || ! configObj.sysadminsMsgConfig) return;
-    var sysadminsMsgConfig=configObj.sysadminsMsgConfig;
-    var system = sysadminsMsgConfig.system ? sysadminsMsgConfig.system : null;
-    var backup = sysadminsMsgConfig.backup ? sysadminsMsgConfig.backup : null;
-
-    disk.check(system, function(err, info) {
-        if (err) {
-            console.log(err);
-        } else {
-            console.log("available /", info.available/1073741824);
-            console.log("free /",info.free/1073741824);
-            console.log("total /",info.total/1073741824);
-            disk.check(backup, function(err, info) {
-                if (err) {
-                    console.log(err);
-                } else {
-                    console.log("available /home", info.available/1073741824);
-                    console.log("free /home", info.free/1073741824);
-                    console.log("total /home",info.total/1073741824);
-                }
-            });
-        }
-    });
-
-}
-
-function getLastBackupFile(){  console.log("getLastBackupFile=");
-    try{
-        var files = fs.readdirSync(path.join(__dirname,'./backup'));
-    }catch(e){
-        console.log("data=",data);
-    }
-    console.log("files[0]=",files[0]);
-    fs.stat(path.join(__dirname,'./backup/'+files[0]), function(err, stat){
-        console.log("err=",err);
-        console.log("stat=",stat);
-    })
+    return configObj;
 }
 
 app.listen(8182);
